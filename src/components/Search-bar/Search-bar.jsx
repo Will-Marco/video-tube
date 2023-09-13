2dc67493591416627fdb60f0b60df3be51485c40
Will-Marco <mr.marco0101@gmail.com>
2023-09-13 20:03:08 +0500

    add Searching

--- src/components/Search-bar/Search-bar.jsx
+++ src/components/Search-bar/Search-bar.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { IconButton, Paper } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import { colors } from "../../constants/constants";
 
 export default function SearchBar() {
+  const navigate = useNavigate();
+  const [value, setValue] = useState("");
+  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate(`./search/${value}`);
+    setValue("");
+  };
+
   return (
     <Paper
       component={"form"}
-      sx={{ border: `1px solid ${colors.secondary}`, pl: 2, boxShadow: "none" }}
+      onSubmit={submitHandler}
+      sx={{
+        border: `1px solid ${colors.secondary}`,
+        pl: 2,
+        boxShadow: "none",
+      }}
     >
-      <input type="text" className="search-bar" placeholder="Search..." />
-      <IconButton>
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <IconButton type="submit">
         <Search />
       </IconButton>
     </Paper>
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Container, Typography } from "@mui/material";
+import { ApiService } from "../../service/api.service";
+import { colors } from "../../constants/constants";
+import { Videos } from "..";
+
 const Search = () => {
-  return <h1>Search</h1>;
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await ApiService.fetching(
+          `search?part=snippet&q=${id}`
+        );
+        setVideos(data.items);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [id]);
+
+  return (
+    <Box p={2} sx={{ height: "90vh" }}>
+      <Container maxWidth={"90%"}>
+        <Typography variant="h4" mb={2} fontWeight={"bold"}>
+          Search results for{" "}
+          <span style={{ color: colors.secondary }}>{id}</span> videos
+        </Typography>
+        <Videos videos={videos}/>
+      </Container>
+    </Box>
+  );
 };
 
 export default Search;
